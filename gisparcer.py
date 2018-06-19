39fe249c4c6db6c38ecf17a31998f6aeeb93e1f3
Count Zero <king.seth@gmail.com>
2018-06-18 22:00:18 -0400

    got zipcode lookup working but running out of queries so need to register.

--- gisparcer.py
+++ gisparcer.py
@@ -1,18 +1,31 @@
 import csv
 import os.path
+from pygeocoder import Geocoder
 
 
 """     
 main
 """
+
 def main():
-    kml_file = "/opt/repos/GIS_parser/Bedford_County_Parcels.csv"
-    if os.path.isfile(kml_file):
+
+    addresskey = "LocAddr"
+    postalkey  = "PostalCode"
+    countystate = "Bedford County, Va"
+    csv_file = "/opt/repos/GIS_parser/Bedford_County_Parcels.csv"
+    if os.path.isfile(csv_file):
         print "Files exists."
-        with open(kml_file, 'rb') as csvfile:
+        with open(csv_file, 'rb') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                print row
+                street = row[addresskey].strip()
+                if addresskey in row and len(street) > 0:
+                    fulladdress = street + ", " + countystate
+                    print fulladdress
+                    result = Geocoder.geocode(fulladdress)
+                    if result.count > 0 and result.administrative_area_level_2 == "Bedford County":
+                        print result.postal_code
+                        row[postalkey] = result.postal_code
     else:
         print "No File :(\n"
 
